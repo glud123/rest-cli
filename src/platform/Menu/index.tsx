1071f1d5c3cbcd134bc740ff9d4aee154be98b9a
高昆 <kun.gao@chaitin.com>
2021-07-15 16:00:50 +0800

    fix：刷新后菜单激活

--- src/platform/Menu/index.tsx
+++ src/platform/Menu/index.tsx
@@ -1,5 +1,5 @@
-import React, { FC } from "react";
-import { useHistory } from "react-router-dom";
+import React, { FC, useMemo } from "react";
+import { useHistory, useLocation } from "react-router-dom";
 import { Menu as AntdMenu } from "antd";
 import { menuList } from "@/route/routes";
 import "./index.less";
@@ -8,6 +8,26 @@ const { SubMenu, ItemGroup, Item: MenuItem } = AntdMenu;
 
 const Menu: FC<any> = () => {
   let history = useHistory();
+  const location = useLocation();
+
+  const defaultOpenKeys = useMemo(() => {
+    let subMenuKey = location.pathname.split("/");
+    subMenuKey = subMenuKey.slice(1, subMenuKey.length - 1);
+
+    subMenuKey = subMenuKey.map((key, i) => {
+      const before = subMenuKey[i - 1];
+      if (before) {
+        return `/${before}/${key}`;
+      } else {
+        return `/${key}`;
+      }
+    });
+    return subMenuKey;
+  }, []);
+
+  const defaultSelectedKeys = useMemo(() => {
+    return [location.pathname];
+  }, []);
 
   const createMenu = (options: any[]) => {
     return options.map((item, index) => {
@@ -22,10 +42,13 @@ const Menu: FC<any> = () => {
       }
     });
   };
+
   return (
     <div className="menu-wrapper">
       <AntdMenu
         mode="inline"
+        defaultSelectedKeys={defaultSelectedKeys}
+        defaultOpenKeys={defaultOpenKeys}
         onClick={(e) => {
           const { key, keyPath } = e;
           console.log(keyPath);
