96288494935385c0bb70891235443e74b06dbf1b
glud123 <glud123@163.com>
2021-07-18 09:56:23 +0800

    fix:优化列表操作逻辑

--- src/components/OperationList/index.tsx
+++ src/components/OperationList/index.tsx
@@ -10,10 +10,7 @@ import "./index.less";
 interface OperationListInterface {
   data: { value: string; [k: string]: any }[];
   activeKey?: string;
-  onChange?: (
-    type: "add" | "del" | "up" | "down",
-    list: { value: string; [k: string]: any }[]
-  ) => void;
+  onChange?: (list: { value: string; [k: string]: any }[]) => void;
   onAdd?: () => void;
   onDelete?: () => void;
   onUp?: () => void;
@@ -46,6 +43,14 @@ const OperationList: FC<OperationListInterface> = (props) => {
     }
   }, [data]);
 
+  useEffect(() => {
+    let newList = list.map((item) => {
+      const { _label, value, ...args } = item;
+      return { value, ...args };
+    });
+    onChange && onChange(newList);
+  }, [list]);
+
   const handleBtnClick = (k: "add" | "del" | "up" | "down") => {
     if (k === "add") {
       setList([
@@ -58,16 +63,41 @@ const OperationList: FC<OperationListInterface> = (props) => {
       setNum(list.length);
     }
     if (k === "del") {
+      if (list.length === 0) return;
+      let nexNum = num;
+      if (nexNum > list.length - 1 && nexNum > 0) {
+        nexNum = nexNum - 1;
+      }
+      console.log(nexNum);
+      list.splice(nexNum, 1);
+      let nextList = list.map((item, index) => {
+        return { ...item, _label: `阶段 ${index + 1} ` };
+      });
+      setList([...nextList]);
+      setNum(num === 0 ? 0 : num - 1);
     }
     if (k === "up") {
+      let preIndex = num - 1 >= 0 ? num - 1 : 0;
+      const preItem = list[preIndex];
+      const _label = preItem._label;
+      list[preIndex] = list[num];
+      list[num] = preItem;
+      list[num]._label = list[preIndex]._label;
+      list[preIndex]._label = _label;
+      setList([...list]);
+      setNum(preIndex);
     }
     if (k === "down") {
+      let nextIndex = num + 1 <= list.length - 1 ? num + 1 : list.length - 1;
+      const nextItem = list[nextIndex];
+      const _label = nextItem._label;
+      list[nextIndex] = list[num];
+      list[num] = nextItem;
+      list[num]._label = list[nextIndex]._label;
+      list[nextIndex]._label = _label;
+      setList([...list]);
+      setNum(nextIndex);
     }
-    let newList = list.map((item) => {
-      const { _label, value, ...args } = item;
-      return { value, ...args };
-    });
-    onChange && onChange(k, newList);
   };
 
   return (
--- src/modules/curriculumDesign/Details/Content.tsx
+++ src/modules/curriculumDesign/Details/Content.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import OperationList from "@/components/OperationList";
 
 const Content = () => {
+  const handleChange = (list: { value: string; [k: string]: any }[]) => {
+    console.log(list);
+  };
   return (
     <div className="curriculum-content">
       <div className="cc-left">
-        <OperationList data={[]} />
+        <OperationList data={[]} onChange={handleChange} />
       </div>
       <div className="cc-right"></div>
     </div>
