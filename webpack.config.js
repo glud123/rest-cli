c2794b7baa9e30981f7168432b3d27823b21a6e2
高昆 <kun.gao@chaitin.com>
2021-07-27 18:42:06 +0800

    fix: 修改开发环境本地服务端口号

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const product = require("./config/prod.js");
 
 const config = {
   host: "127.0.0.1",
-  port: "8089",
+  port: "8088",
   proxy: {
     "/cts": "http://82.156.254.84:8080/cts/",
   },
